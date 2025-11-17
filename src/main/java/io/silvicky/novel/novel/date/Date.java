3726cb34269641a57c0638035a13b247aba6c627
Vicky Silviana <vickyisnotaboy@gmail.com>
2025-11-17 20:20:44 +0800

    date stub

---
+++ src/main/java/io/silvicky/novel/novel/date/Date.java
@@ -0,0 +1,6 @@
+package io.silvicky.novel.novel.date;
+
+public class Date
+{
+
+}
---
+++ src/main/java/io/silvicky/novel/novel/date/DateShuffler.java
@@ -0,0 +1,20 @@
+package io.silvicky.novel.novel.date;
+
+import java.util.Date;
+import java.util.Random;
+
+public class DateShuffler
+{
+    private final long seed;
+    public DateShuffler(long seed)
+    {
+        this.seed=seed;
+        Random random=new Random(seed);
+        //TODO
+    }
+    public Date getObfuscatedDate(Date input)
+    {
+        //TODO
+        return null;
+    }
+}
